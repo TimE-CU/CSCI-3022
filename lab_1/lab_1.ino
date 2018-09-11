a520dd579274c73f815cf29f52186b0064ba163f
Tim Ewing <timjewing@gmail.com>
2018-09-10 20:55:57 -0600

    Working version of lab 1

--- lab_1/lab_1.ino
+++ lab_1/lab_1.ino
@@ -28,10 +28,12 @@ void setup() {
 }
 
 void readSensors() {
-  cm_distance = 0; // Replace with code to read the distance sensor
-  line_left = 0; // Replace with code to read the left IR sensor
-  line_right = 0; // Replace with code to read the right IR sensor
-  line_center = 0; // Replace with code to read the center IR sensor
+  // Read untrasonic
+  cm_distance = sparki.ping(); 
+  sparki.println(cm_distance);
+  line_left = sparki.lineLeft(); // Replace with code to read the left IR sensor
+  line_right = sparki.lineRight(); // Replace with code to read the right IR sensor
+  line_center = sparki.lineCenter(); // Replace with code to read the center IR sensor
 }
 
 void loop() {
@@ -42,26 +44,76 @@ void loop() {
   sparki.print("STATE: ");
   sparki.println(current_state);
 
+  sparki.println("center, left, right");
+  sparki.println(line_center);
+  sparki.println(line_left);
+  sparki.println(line_right);
+
   // Your state machine code goes here
 
   switch(current_state) {
     case STATE_FIND_OBJ:
       // Rotate until object is found
+      sparki.moveRight();
+      if(cm_distance <= 30 && cm_distance != -1) {
+        sparki.moveStop();
+        current_state = STATE_DRIVE_TO_OBJ;
+      }
+        
       break;
     case STATE_DRIVE_TO_OBJ:
       // Drive Within 7cm of object
+      sparki.moveForward(cm_distance - 7);
+      current_state = STATE_GRAB_OBJ;
+      
       break;
     case STATE_GRAB_OBJ:
       // Grab object w pinchers
+      sparki.gripperClose(6);
+      delay(4000);
+      sparki.moveRight(180);
+      current_state = STATE_FIND_LINE;
       break;
     case STATE_FIND_LINE:
       // Turn 180 and drive until a line is found
+      sparki.println("find line");
+      
+      sparki.moveForward();
+
+      if(line_center < threshold ) {
+        sparki.moveStop();
+        current_state = STATE_FOLLOW_LINE;
+      }
       break;
     case STATE_FOLLOW_LINE:
       // Follow the line until "start"
+
+      // IF start line is found change state
+      if(line_right < threshold && line_left < threshold && line_center < threshold) {
+        current_state = STATE_FOUND_START;
+      }
+
+      
+      if (line_center < threshold) {
+        sparki.moveForward();
+      }
+      else if (line_left < threshold) 
+      {  
+        sparki.moveLeft();
+      }
+      else if(line_right < threshold) 
+      {  
+        sparki.moveRight(); 
+      }
+      
+      
       break;
     case STATE_FOUND_START:
       // beep and drop object
+      sparki.moveStop();
+      sparki.RGB(RGB_RED);
+      sparki.beep(1);
+      sparki.gripperOpen();
       break;
     default:
       break;
